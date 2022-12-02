05b621c41829e99e4df70119f65df6e38f2f449e
José Postiga <jpostiga@infraspeak.com>
2022-12-02 07:50:22 +0000

    Show both answers on output

--- main.go
+++ main.go
@@ -7,5 +7,6 @@ func main() {
 	for _, itemsCalories := range ParseCaloriesInput() {
 		podium.handle(itemsCalories.calculate())
 	}
+	fmt.Printf("High value of calories input is: %d\n", podium.first)
 	fmt.Printf("Total calories count from top 3 Elses is: %d\n", podium.sum())
 }
